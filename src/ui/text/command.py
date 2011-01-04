cfc540d944df8676da48d33b08fef505c6fb0c0a
franck <franck.vdl@gmail.com>
2011-01-04 18:00:43 +0100

    Add chord diagram viewer. Move tab viewer to submodule.

--- src/ui/text/command.py
+++ src/ui/text/command.py
@@ -5,6 +5,8 @@ from mingus.core import scales, chords
 from guitar import Guitar
 from parsers import ReaderFactory
 
+from . import tab, chord
+
 def do_scale(options, scale):
     g = Guitar(frets=options.frets)
     scope = get_scope(options, g)
@@ -50,61 +52,17 @@ def do_tab(options, filename):
     reader = ReaderFactory.getReader(filename)
     reader.open()
     song = reader.parseSong()
-    track = song.tracks[options.track]
     scope = [min(int(n), len(song.measures)) for n in options.range.split(',')]
 
-    for subscope in split_scope(scope, 5):
-        show_tab(track, subscope)
+    tab.show_tab(song, options.track, scope)
+
     reader.close()
 
 def do_diagram(options, notes):
-    pass
+    notes = notes.lower().replace('x','-1')
+    frets = [int(f) for f in notes.split(',')]
 
-def split_scope(scope, r):
-    inf = scope[0]
-    sup = scope[1]
-    result = []
-    
-    i = inf
-    while i<sup:
-        m = min(sup, i+r)
-        result.append((i, m))
-        i += r
-
-    return result
-
-def show_tab(track, measures=(0, 5)):
-    strings = ['|' for i in range(track.nstrings)]
-    for measure in track.measures[measures[0]:measures[1]]:
-        for beat in measure.beats:
-            for i in range(track.nstrings):
-                note = None
-                for n in beat.notes:
-                    if n.string == i+1:
-                        note = n
-                if note is None or note.tied:
-                    strings[i] += '--'
-                elif note.dead:
-                    strings[i] += 'x-'
-                else:
-                    strings[i] += str(note.fret)
-                    if note.fret < 10:
-                        strings[i] += '-'
-
-                if note and note.hammer:
-                    strings[i] += 'h'
-                elif note and note.slide:
-                    strings[i] += '/'
-                elif note and note.bend > 0:
-                    strings[i] += '^'
-                else:
-                    strings[i] += '-'
-        for i in range(track.nstrings):
-            strings[i] += '|'
-
-    for string in strings:
-        print(string)
-    print()
+    chord.show_chord(frets)
 
 def get_scope(options, g):
     if options.range:
